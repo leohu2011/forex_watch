5163d3b3be018b186e4a3357310d7fc0c9e1a228
huqi <leo-miracle@hotmail.com>
2017-05-04 07:26:36 -0400

    Initial Commit

---
+++ forex_watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  forex_watch
+//
+//  Created by Qi Hu on 4/5/17.
+//  Copyright © 2017 Qi Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
