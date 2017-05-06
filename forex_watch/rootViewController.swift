efc8ed062a7fbe8566525a229b8ba962d7429716
developer <developer@Hes-MacBook-Pro.lan>
2017-05-06 08:23:03 -0400

    font adjustment

---
+++ forex_watch/rootViewController.swift
@@ -0,0 +1,35 @@
+//
+//  rootViewController.swift
+//  forex_watch
+//
+//  Created by developer on 2017-05-06.
+//  Copyright © 2017 Qi Hu. All rights reserved.
+//
+
+import UIKit
+
+class rootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
